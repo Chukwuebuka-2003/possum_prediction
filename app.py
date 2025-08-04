d37733f3b2149c452bb3b532e855246af311f2cf
saivenkatak33 <kolukuluriraju33@gmail.com>
2025-08-04 22:39:46 +0530

    Target variable name changed now predicting to possum belly

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ with open('possum.sav', 'rb') as model_file:
 def predict():
     data = request.json
 
-    required_params = ['skullw', 'totlngth', 'footlngth', 'belly', 'chest', 'eye', 'age']
+    required_params = ['skullw', 'totlngth', 'footlngth', 'hdlngth', 'chest', 'eye', 'age']
     if not all(param in data for param in required_params):
         return jsonify({'error': 'Missing required parameters'})
 
     # Perform prediction using the loaded model
     try:
-        prediction = model.predict([[data['skullw'], data['totlngth'], data['footlngth'], data['belly'], data['chest'], data['eye'], data['age']]])
+        prediction = model.predict([[data['skullw'], data['totlngth'], data['footlngth'], data['hdlngth'], data['chest'], data['eye'], data['age']]])
         # Return prediction as part of response
         return jsonify({'prediction': float(prediction[0])})
     except Exception as e:
@@ -25,3 +25,4 @@ def predict():
 
 if __name__ == '__main__':
     app.run(debug=True)
+ 
